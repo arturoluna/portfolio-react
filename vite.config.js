e067876e77dadbb29702509fb3a300519e6cb961
arturoluna <art@arturoluna.com>
2025-05-29 18:18:18 -0700

    init repo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-react/', // ← update this based on your repo name
   plugins: [react()],
-})
+});
